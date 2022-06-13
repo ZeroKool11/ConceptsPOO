1ff768c2c38eb02569dbcdeb9201eabf72f7e13b
dsilva1 <dany.silva@realpage.com>
2022-06-13 18:48:48 -0500

    Fixed dates

--- ConceptsPOO/Program.cs
+++ ConceptsPOO/Program.cs
@@ -8,7 +8,7 @@ try
 
     Console.WriteLine(new Date(2024, 2, 29));
     Console.WriteLine(new Date(1974, 9, 23));
-    Console.WriteLine(new Date(1985, 11, 31));
+    Console.WriteLine(new Date(1985, 11, 30));
 }
 catch (Exception error)
 {
