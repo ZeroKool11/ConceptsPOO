3debbbde781303b22f2636c6835326e9a5c2e12c
dsilva1 <dany.silva@realpage.com>
2022-06-15 21:14:44 -0500

    Employee, SalaryEmployee & CommisionEmployee finish

--- ConceptsPOO/Program.cs
+++ ConceptsPOO/Program.cs
@@ -3,15 +3,28 @@ using ConceptsPOO;
 
 Console.WriteLine("POO Concepts");
 Console.WriteLine("============");
-try
-{
 
-    Console.WriteLine(new Date(2024, 2, 29));
-    Console.WriteLine(new Date(1974, 9, 23));
-    Console.WriteLine(new Date(1985, 11, 30));
-}
-catch (Exception error)
+Employee employee1 = new SalaryEmployee()
 {
-    Console.WriteLine(error.Message);
-    
-}
+    Id = 1010,
+    FirstName = "Sandra",
+    LastName = "Morales",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 1815453.45M
+};
+Console.WriteLine(employee1);
+
+Employee employee2 = new CommissionEmployee()
+{
+    Id = 2020,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 2, 5),
+    IsActive = true,
+    Sales = 320000000M,
+    CommissionPercentage = 0.03F
+};
+Console.WriteLine(employee2);
